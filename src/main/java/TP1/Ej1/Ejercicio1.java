47c3895bd8b0d70c1fb37f3093558ea94a8f5e2c
jonathan Hiriart <hiriartjonathan@gmail.com>
2025-03-28 10:47:21 -0300

    terminando ejecicio 1

--- src/main/java/TP1/Ej1/Ejercicio1.java
+++ src/main/java/TP1/Ej1/Ejercicio1.java
@@ -1,4 +1,37 @@
 package TP1.Ej1;
-
+import java.util.Scanner;
 public class Ejercicio1 {
+
+    public static void ImprimirFor(int a,int b ) {
+        for (int i=a; i<=b; i++) {
+            System.out.print(i + " ");
+        }
+    }
+    public static void ImprimirWhile(int a,int b ) {
+        while(a<=b){
+            System.out.print(a+ " ");
+            a++;
+        }
+    }
+    public static void ImprimirSinEstruc(int a,int b){
+        if (a<=b){
+            System.out.print(a+ " ");
+            a++;
+            ImprimirSinEstruc(a,b);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el primer numero: ");
+        int a = sc.nextInt();
+        System.out.print("Ingrese el segundo numero: ");
+        int b = sc.nextInt();
+        ImprimirFor(a,b);
+        System.out.println(" ");
+        ImprimirWhile(a,b);
+        System.out.println(" ");
+        ImprimirSinEstruc(a,b);
+    }
 }
+
